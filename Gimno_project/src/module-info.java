f98d188ee53fa400e4d5d26009296dbf0442b0c3
Delfa Fdes <fernandesdelfa1234@gmail.com>
2024-01-08 09:47:13 +0530

    first github commit

---
+++ Gimno_project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Gimno_project {
+}
---
+++ Gimno_project/src/sybcapro/helloworld.java
@@ -0,0 +1,11 @@
+package sybcapro;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" HELLO WORLD ");
+	}
+
+}
